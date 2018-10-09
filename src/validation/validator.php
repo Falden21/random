7de872c16b1cca0e5830228cdd90d21c14a4f9b1
stephen <faldennope@gmail.com>
2018-10-09 06:24:45 -0500

    simplified code

--- src/validation/validator.php
+++ src/validation/validator.php
@@ -15,41 +15,39 @@ class validator
     public function isValid($vd)
     {
         $err = [];
-        foreach ($vd as $name => $value) {
-            if (isset($_REQUEST[$name])) {
-                $rules = explode('|', $value);
-                foreach ($rules as $rule) {
-                    $exploded = explode(':', $rule);
-                    switch ($exploded[0]) {
-                        case
-                        'min':
-                            $min = $exploded[1];
-                            if (Valid::stringType()->length($min)->validate($_REQUEST[$name]) == false) {
-                                $err[] = "{$name} is required to be at least {$min} characters long.";
-                            }
-                            break;
-                        case
-                        'email':
-                            if (Valid::email()->validate($_REQUEST[$name]) == false) {
-                                $err[] = "{$name} is required to be an email.";
-                            }
-                            break;
+        // getting an array, ["first_name" => "min:3", "last_name" => "min:3"]
+        if (isset($_REQUEST[$name])) {
+            $rules = explode('|', $value);
+            foreach ($rules as $rule) {
+                $exploded = explode(':', $rule);
+                switch ($exploded[0]) {
+                    case
+                    'min':
+                        $min = $exploded[1];
+                        if (Valid::stringType()->length($min)->validate($_REQUEST[$name]) == false) {
+                            $err[] = "{$name} is required to be at least {$min} characters long.";
+                        }
+                        break;
+                    case
+                    'email':
+                        if (Valid::email()->validate($_REQUEST[$name]) == false) {
+                            $err[] = "{$name} is required to be an email.";
+                        }
+                        break;
 
-                        case
-                        "equalTo":
-                            if (Valid::equals($_REQUEST[$name])->validate($_REQUEST[$exploded[1]]) == false) {
-                                $verifyer = str_replace('_', ' ', $exploded[1]);
-                                $err[]    = "{$name} is required to match {$verifyer}";
-                            }
-                            break;
+                    case
+                    "equalTo":
+                        if (Valid::equals($_REQUEST[$name])->validate($_REQUEST[$exploded[1]]) == false) {
+                            $verifyer = str_replace('_', ' ', $exploded[1]);
+                            $err[]    = "{$name} is required to match {$verifyer}";
+                        }
+                        break;
 
-                        default:
-                            // Do nothing
+                    default:
+                        // Do nothing
+                        return $err = ["No value found."];
 
-                    }
                 }
-            } else {
-                return $err = ["No value found."];
             }
         }
         return $err;
